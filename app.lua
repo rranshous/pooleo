4bd4c5097a7faa6f04f9dfc5f01d71943e6b18bb
Robby Ranshous <rranshous@gmail.com>
2015-04-11 19:12:28 -0400

    async requests

--- app.lua
+++ app.lua
@@ -10,62 +10,90 @@ local url = require "net.url"
 -- get the json from the request body
 local body = ngx.req.read_body()
 if not body then
-  ngx.log(ngx.DEBUG, "getting body from cache")
   body = ngx.req.get_body_data()
 end
-ngx.log(ngx.INFO, "BODY: ", body)
 
 local body_data = cjson.decode(body)
 
--- pull the href from the bodies data { "href" : "" }
-local href = body_data["href"]
-ngx.log(ngx.INFO, "HREF: ", href)
+-- search for hrefs
+function href_find (tbl, path, found)
+  if not found then
+    found = {}
+  end
+  if not path then
+    path = '/'
+  end
+  for k,v in pairs(tbl) do
+    if k == "href" then
+      found[path] = v
+    end
+    if type(v) == "table" then
+      found = href_find(v, path .. '/' .. k, found)
+    end
+  end
+  return found
+end
+local hrefs = href_find(body_data, "/", nil)
 
--- parse our URL
-local href_details = url.parse(href)
-local host = href_details.host
-local path = href_details.path
-ngx.log(ngx.INFO, "HOST: ", host)
-ngx.log(ngx.INFO, "PATH: ", path)
+-- connect to each href's server
+local responses = {}
+for data_path, href in pairs(hrefs) do
+  -- parse our URL
+  local href_details = url.parse(href)
 
--- connect to the host
-httpc:set_timeout(500)
-httpc:connect(href_details["host"], 80)
+  -- connect to the host
+  httpc:set_timeout(500)
+  httpc:connect(href_details["host"], 80)
 
--- make our request out
-local res, err = httpc:request{
-  path = href_details["path"],
-  headers = {
-    ["Host"] = host
-  },
-}
-
-if not res then
-  ngx.say("failed to request: ", err)
-  return
+  -- start our request
+  local res, err = httpc:request{
+    path = href_details["path"],
+    headers = {
+      ["Host"] = href_details["host"]
+    },
+  }
+  -- keep track of our responses
+  responses[href] = {}
+  responses[href].res = res
+  responses[href].err = err
 end
 
--- Now we can use the body_reader iterator, to stream the body according to our desired chunk size.
-local reader = res.body_reader
+-- go through the responses
+for href, details in pairs(responses) do
+  local res = details.res
+  local err = details.err
 
-repeat
-  local chunk, err = reader(8192)
-  if err then
-    ngx.log(ngx.ERR, err)
-    break
+  if not res then
+    ngx.say("failed to request[", href, "]: ", err)
+    responses[href] = cjson.encode({ error=err })
+  else
+
+    -- TODO, stream as available instead of starting from first req
+    -- Now we can use the body_reader iterator, to stream the body
+    -- according to our desired chunk size.
+    local reader = res.body_reader
+
+    responses[href] = ""
+    repeat
+      local chunk, err = reader(8192)
+      if err then
+        ngx.log(ngx.ERR, err)
+        break
+      end
+
+      if chunk then
+        responses[href] = responses[href] .. chunk
+      end
+    until not chunk
+
+    -- local ok, err = httpc:set_keepalive()
+    -- if not ok then
+    --   ngx.say("failed to set keepalive: ", err)
+    --   return
+    -- end
   end
-
-  if chunk then
-    ngx.say(chunk)
-  end
-until not chunk
-
-local ok, err = httpc:set_keepalive()
-if not ok then
-  ngx.say("failed to set keepalive: ", err)
-  return
 end
 
+ngx.say(cjson.encode(responses))
 
--- ngx.say('Hello World!')
--- ngx.exit(200)
+ngx.exit(200)
